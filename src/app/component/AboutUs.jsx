a48751d703dc8556116818c9b91a2f385398ccf8
Ahmed <lameda789@gmail.com>
2024-12-31 15:12:23 +0100

    update

--- src/app/component/AboutUs.jsx
+++ src/app/component/AboutUs.jsx
@@ -5,17 +5,19 @@ const AboutUs = () => {
     <div id="about-us" className="grid grid-cols-1 md:grid-cols-2">
       {/* Left Section */}
       <div className="bg-[#027839] text-white p-8 md:p-16">
-        <h1 className="text-2xl md:text-3xl font-medium mb-6 md:mb-9">
-          About Us
-        </h1>
-        <p className="text-sm leading-6 md:leading-7">
-          The Fatimah Bolanle Abioye Foundation (FBA Foundation) is a non-profit
-          organization dedicated to transforming lives through educational
-          empowerment and humanitarian aid in Nigeria. Named after Alhaja
-          Fatimah Bolanle Abioye, whose lifelong commitment to helping others
-          despite limited resources has inspired this initiative, our foundation
-          continues her legacy of compassion and generosity.
-        </p>
+        <div className="flex flex-col justify-center h-full">
+          <h1 className="text-2xl md:text-3xl font-medium mb-6 md:mb-9">
+            About Us
+          </h1>
+          <p className="text-sm leading-6 md:leading-7">
+            The Fatimah Bolanle Abioye Foundation (FBA Foundation) is a
+            non-profit organization dedicated to transforming lives through
+            educational empowerment and humanitarian aid in Nigeria. Named after
+            Alhaja Fatimah Bolanle Abioye, whose lifelong commitment to helping
+            others despite limited resources has inspired this initiative, our
+            foundation continues her legacy of compassion and generosity.
+          </p>
+        </div>
       </div>
 
       {/* Right Section */}
