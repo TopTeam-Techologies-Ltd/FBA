a7e2003d010c7153c61e54d4121ce45e1fc184e8
Ahmed <lameda789@gmail.com>
2024-12-31 11:21:47 +0100

    update

--- src/app/component/VisionStatement.jsx
+++ src/app/component/VisionStatement.jsx
@@ -15,47 +15,78 @@ const VisionStatement = () => {
         </p>
       </div>
       <div className="w-[90%] mx-auto">
-        <div className="grid grid-cols-12 gap-2">
-          <div className="col-span-3 p-2">
-            <div className="flex flex-col gap-2">
+        <div className="grid grid-cols-12">
+          <div className="col-span-3 p-2 border-r border-b border-[#027839]">
+            <div className="flex flex-col gap-6 p-4">
+              <h1 className="text-2xl">Mission Statement</h1>
               <div>
-                <h1 className="text-2xl">Mission Statement</h1>
-                <div>
-                  <Image
-                    src={"/arrow.png"}
-                    alt="arrow pointing right"
-                    width={200}
-                    height={40}
-                  />
-                </div>
-              </div>
-              <div className="border-t-2 border-[#272727]">
-                <h1 className="text-2xl">Our Core Values</h1>
-                <div>
-                  <Image
-                    src={"/arrow.png"}
-                    alt="arrow pointing right"
-                    width={200}
-                    height={40}
-                  />
-                </div>
+                <Image
+                  src={"/arrow.png"}
+                  alt="arrow pointing right"
+                  width={200}
+                  height={40}
+                />
               </div>
             </div>
           </div>
-          <div className="col-span-3 p-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ad
-            quibusdam rerum esse, ratione suscipit voluptatem nemo officiis
-            sequi minima.
+          <div className="col-span-3 p-2 border-r border-b border-[#027839] flex justify-center items-center bg-[#FFB50A] text-center">
+            Providing educational opportunities and resources to underprivileged
+            youth in Nigeria
+          </div>
+          <div className="col-span-3 p-2 border-r border-b border-[#027839] flex justify-center items-center text-center">
+            Supporting vulnerable communities through targeted aid programs
+          </div>
+          <div className="col-span-3 p-2 border-b border-[#027839] flex justify-center items-center text-center">
+            Creating sustainable pathways out of poverty through knowledge and
+            skill development
+          </div>
+        </div>
+        <div className="grid grid-cols-12">
+          <div className="col-span-3 p-2 border-r  border-[#027839]">
+            <div className="flex flex-col gap-6 p-4">
+              <h1 className="text-2xl">Our Core Values</h1>
+              <div>
+                <Image
+                  src={"/arrow.png"}
+                  alt="arrow pointing right"
+                  width={200}
+                  height={40}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3 p-2 border-r border-[#027839] flex justify-center items-center">
+            <div className="flex flex-col gap-2">
+              <h1 className="font-medium text-[#272727] text-center">
+                Compassion:
+              </h1>
+              <p className="text-center">
+                Following the example of our namesake, we lead with empathy and
+                understanding
+              </p>
+            </div>
+          </div>
+          <div className="col-span-3 p-2 border-r border-[#027839]">
+            <div className="flex flex-col gap-2">
+              <h1 className="font-medium text-[#272727] text-center">
+                Integrity:
+              </h1>
+              <p className="text-center">
+                Maintaining transparency and accountability in all our
+                operations
+              </p>
+            </div>
           </div>
           <div className="col-span-3 p-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ad
-            quibusdam rerum esse, ratione suscipit voluptatem nemo officiis
-            sequi minima.
-          </div>
-          <div className="col-span-3 p-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ad
-            quibusdam rerum esse, ratione suscipit voluptatem nemo officiis
-            sequi minima.
+            <div className="flex flex-col gap-2">
+              <h1 className="font-medium text-[#272727] text-center">
+                Excellence:
+              </h1>
+              <p className="text-center">
+                Striving for the highest standards in our programs and service
+                delivery
+              </p>
+            </div>
           </div>
         </div>
       </div>
