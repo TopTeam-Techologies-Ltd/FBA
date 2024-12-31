268923826690f0410325d343812c6f4b478583bb
Ahmed <lameda789@gmail.com>
2025-01-01 00:20:39 +0100

    update

--- src/app/component/Navbar.jsx
+++ src/app/component/Navbar.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
 import Link from "next/link";
-
+3;
 const Navbar = () => {
   return (
     <>
       <div className="w-full bg-white fixed shadow-md flex justify-between items-center px-16 py-3 z-50">
-        <div className="flex item-center gap-2">
+        {/* <div className="flex item-center gap-2">
           <Image
             src={"/logo.png"}
             alt="Foundation Building Africa Logo"
@@ -18,7 +18,11 @@ const Navbar = () => {
             <p className="font-semibold text-[#000066] text-sm">Foundation</p>
             <p className="text-xs">The Fatima Bolanle Abioye Foundation</p>
           </div>
-        </div>
+        </div> */}
+        {/* testing something here */}
+        <div className="h-[50px] w-[200px] bg-[url('/fba-logo3.jpg')] bg-cover bg-center bg-no-repeat"></div>
+        {/* <div className="h-[60px] w-[200px] bg-[url('/fba-logo2.png')] bg-cover bg-center bg-no-repeat"></div> */}
+
         <div>
           <ul className="flex space-x-10">
             <li>
