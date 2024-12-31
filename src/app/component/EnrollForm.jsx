3602b9174f4150a18ecb594c8e5928a0ee948210
Ahmed <lameda789@gmail.com>
2024-12-31 15:06:46 +0100

    update

--- src/app/component/EnrollForm.jsx
+++ src/app/component/EnrollForm.jsx
@@ -109,8 +109,8 @@ export default function EnrollForm({ setOpen }) {
   return (
     <>
       <form
-        onSubmit={handleSubmit}
-        className="w-[50rem] mx-auto p-6 bg-white shadow-lg rounded-lg mb-10 relative"
+        // onSubmit={handleSubmit}
+        className="w-[40rem] mx-auto p-6  rounded-lg mb-10 relative"
       >
         <h1 className="text-center mb-6 p-2 bg-[#33af67] text-white font-semibold rounded-md">
           Kindly Fill in the Information Below
@@ -146,55 +146,22 @@ export default function EnrollForm({ setOpen }) {
             <option value="soc">Security Operation Analyst</option>
             <option value="cloud-azure">Cloud computing with azure</option>
             <option value="cloud-aws">Cloud computing with aws</option>
-            <option value="cloud-azure-intermediate">Cloud computing with azure (Intermediate level)</option>
+            <option value="cloud-azure-intermediate">
+              Cloud computing with azure (Intermediate level)
+            </option>
           </select>
 
           {/* Name Field with Tooltip */}
-          <div className="relative">
+          <div>
             <input
               type="text"
-              placeholder="Name"
+              placeholder="Full Name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              onFocus={() =>
-                setTooltip("personal info will not be made public.")
-              }
-              onBlur={() => setTooltip("")}
               required
               className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#33af67]"
             />
-            {tooltip && (
-              <div className="absolute top-full mt-1 bg-gray-100 border border-gray-300 p-2 rounded-md shadow-md text-xs">
-                {tooltip}
-              </div>
-            )}
           </div>
-
-          {/* Bedrooms Dropdown */}
-          <select
-            value={bedrooms}
-            onChange={(e) => setBedrooms(e.target.value)}
-            className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#33af67]"
-          >
-            <option value="" disabled>
-              Select Bedrooms
-            </option>
-            {[1, 2, 3, 4, 5, 6].map((num) => (
-              <option key={num} value={num}>
-                {num}
-              </option>
-            ))}
-          </select>
-
-          {/* Budget Field (Number) */}
-          <input
-            type="number"
-            placeholder="Budget"
-            value={budget}
-            onChange={(e) => setBudgets(e.target.value)}
-            required
-            className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#33af67]"
-          />
 
           {/* Phone Number Field with Tooltip */}
           <div className="relative">
@@ -203,18 +170,9 @@ export default function EnrollForm({ setOpen }) {
               placeholder="Phone Number"
               value={phoneNumber}
               onChange={(e) => setPhoneNumber(e.target.value)}
-              onFocus={() =>
-                setTooltip("personal info will not be made public.")
-              }
-              onBlur={() => setTooltip("")}
               required
               className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#33af67]"
             />
-            {tooltip && (
-              <div className="absolute top-full mt-1 bg-gray-100 border border-gray-300 p-2 rounded-md shadow-md text-xs">
-                {tooltip}
-              </div>
-            )}
           </div>
 
           {/* State Dropdown */}
@@ -225,27 +183,13 @@ export default function EnrollForm({ setOpen }) {
             className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#33af67]"
           >
             <option value="" disabled>
-              Select State
+              State
             </option>
             {statesInNigeria.map((stateName) => (
               <option key={stateName} value={stateName}>
                 {stateName}
               </option>
             ))}
-          </select>
-
-          {/* Type of Individual Dropdown */}
-          <select
-            value={typeofindividual}
-            onChange={(e) => setTypeOfIndividual(e.target.value)}
-            required
-            className="p-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#33af67]"
-          >
-            <option value="" disabled>
-              Who is Asking
-            </option>
-            <option value="Agent">Agent</option>
-            <option value="Individual">Individual</option>
           </select>
 
           {/* Email Field with Tooltip */}
@@ -255,10 +199,6 @@ export default function EnrollForm({ setOpen }) {
               placeholder="Email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              onFocus={() =>
-                setTooltip("personal info will not be made public.")
-              }
-              onBlur={() => setTooltip("")}
               required
               className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:border-[#33af67]"
             />
@@ -271,7 +211,7 @@ export default function EnrollForm({ setOpen }) {
 
           {/* Comment Textarea */}
           <textarea
-            placeholder="Comment"
+            placeholder="Kindly tell us why you need this scholarship"
             value={comment}
             onChange={(e) => setComment(e.target.value)}
             required
--- src/app/component/GetInvolved.jsx
+++ src/app/component/GetInvolved.jsx
@@ -18,7 +18,9 @@ const GetInvolved = () => {
         <div className="md:col-span-3">
           <div className="flex flex-col gap-2 md:gap-4 text-[#333030]">
             <h2 className="font-medium text-base md:text-lg">Email Address</h2>
-            <p className="text-sm">fbafound@gmail.com</p>
+            <p className="text-sm">
+              <a href="mailto:info@fbafound.org" className="hover:text-[#000066]">Info@fbafound.org</a>
+            </p>
           </div>
         </div>
 
@@ -38,9 +40,9 @@ const GetInvolved = () => {
               <IconButton aria-label="Instagram">
                 <Instagram fontSize="small" />
               </IconButton>
-              <IconButton aria-label="Facebook">
+              {/* <IconButton aria-label="Facebook">
                 <Facebook fontSize="small" />
-              </IconButton>
+              </IconButton> */}
               <IconButton aria-label="Other">
                 <XIcon fontSize="small" />
               </IconButton>
