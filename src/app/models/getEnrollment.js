879c3deae0fc5088548c47a2100d15d3064797f7
Ahmed <lameda789@gmail.com>
2025-01-10 22:19:38 +0100

    update

--- src/app/models/getEnrollment.js
+++ src/app/models/getEnrollment.js
@@ -2,7 +2,7 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
 
 export async function getEnrollment() {
-  const postsCollection = collection(db, "enroll");
+  const postsCollection = collection(db, "cohort2");
   const snapshot = await getDocs(postsCollection);
   return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 }
