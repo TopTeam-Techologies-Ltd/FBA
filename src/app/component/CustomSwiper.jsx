7de99c64072a859b6c2a072c25216c62670bceea
Ahmed <lameda789@gmail.com>
2025-01-10 12:28:13 +0100

    new update

--- src/app/component/CustomSwiper.jsx
+++ src/app/component/CustomSwiper.jsx
@@ -4,14 +4,24 @@ import { Swiper as SwiperComponent, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 // Import required modules
-import { Navigation } from "swiper/modules"; // Correct import for Swiper 11+
+import { Navigation, Autoplay } from "swiper/modules"; // Correct import for Swiper 11+
+import "swiper/css/autoplay";
 
 import FirstCohort from "./FirstCohort";
 import SecondCohort from "./SecondCohort";
 
 const CustomSwiper = () => {
   return (
-    <SwiperComponent navigation={true} modules={[Navigation]}>
+    <SwiperComponent
+      navigation={true}
+      modules={[Navigation, Autoplay]}
+      loop={true}
+      autoplay={{
+        delay: 5000, // Delay between slides in milliseconds
+        disableOnInteraction: false, // Keep autoplay running after interaction
+        pauseOnMouseEnter: true, // Pause autoplay when the user hovers
+      }}
+    >
       <SwiperSlide>
         <FirstCohort />
       </SwiperSlide>
